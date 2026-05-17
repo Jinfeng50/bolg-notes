b2185c80f2dc1ab972fefcceead12660ba3e65b7
Yanzong Chan <3267189544@qq.com>
2026-05-17 14:48:15 +0800

    fix vitepress base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
-export default defineConfig({
+export default defineConfig( {
+  base: '/bolg-notes/',
   title: 'Jinfeng Notes',
   description: 'Personal technical notes and essays',
 
